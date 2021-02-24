cac0c6db0aaa7c7389daafca331891469daa5f16
ruedi kneubuehler <pavian57@gmx.net>
2021-02-24 15:23:14 +0100

    There were still no all caracters due to my morse class

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a small program in Micropython on esp8266 to run a kind of qso server for Morserino-32. https://github.com/oe1wkl/Morserino-32
 
-i used VS Code with PyMakr to Upload Code to a esp8266 Wemos mini D1. Adjust your Wifi Settings. Start your Morserino, send 'hi' and it will send you back a random callsign, default lenght ist 3 for starters, you may change by your own, or send a number **i >= 3 or i >=8** before sending **cq**, followed with ur rst and 3 times a report. 
+i used VS Code with PyMakr to Upload Code to a esp8266 Wemos mini D1. Adjust your Wifi Settings. Start your Morserino in WifiTrx and follow the procedure below. The qsosrv sends back a random callsign, lenght=3 at the moment, you may change by your own, or send a number **i >= 3 or i >=8** before sending **cq**. 
 
 I will add more features from time to time.
 
@@ -30,7 +30,9 @@ you: rr tu 73 de **yourcallsign** ee
 entering **sk** goes back to step 0
 
 
+on a esp8266 **qsosrv** blinks after every step **.-.** **roger**. 
 
+It runs on Linux(tested on Raspberry Pi 4), Windows (Python 3.9 on Windows 10) and last but not least on a esp8266 with Micropython, this allows you to be portable with Morserino and esp8266.
 
 You're invited to help out with this small project.
 
--- callsign.py
+++ callsign.py
@@ -3,14 +3,12 @@ from random import *
 class CallGenerator():
 
     
-#    chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    chars = ['a','c','d','e','g','h','i','j','k','l','m','n','o','q','r','s','t','u','v','x','y']
-    #0-25  // 0-20
+    chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    #0-25 
     numbers = ['1','2','3','4','5','6','7','8','9','n']
     #0-9
-#    allchars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
-    allchars = ['a','c','d','e','g','h','i','j','k','l','m','n','o','q','r','s','t','u','v','x','y','0','1','2','3','4','5','6','7','8','9']
-    #0-35  // 0-30
+    allchars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+    #0-35
 
    
 
@@ -49,9 +47,9 @@ class CallGenerator():
 
     def get_call(self):
         cs = ''
-        cs = self.chars[self.__randrange(0,20) ]
+        cs = self.chars[self.__randrange(0,25) ]
         for i in range(self.call_length-1):
-            cs += self.allchars [self.__randrange(0,30) ]
+            cs += self.allchars [self.__randrange(0,35) ]
         return cs 
         
 
